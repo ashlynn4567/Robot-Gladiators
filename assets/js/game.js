6fefb62b6c5ea64e733a25d87261c3aa4596bc69
Ashley's PC <ashlynn4567@gmail.com>
2022-02-19 12:10:06 -0600

    introducing basic functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// using a prompt to ask the user for the robot's name
+var playerName = window.prompt("What is your robot's name?");
+//logging the variable stored in the console for us to see what's going on
+console.log(playerName + " is ready for battle!")
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+} 
+
+// this calls our function "fight" so that it runs on our page.
+fight();
